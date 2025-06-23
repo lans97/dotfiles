d19d75b8169f05c347e46c47a1a5715897875101
Luis Silva <luis.silva1mon@gmail.com>
2025-06-23 09:19:20 -0600

    [ADD] PG_Format
    Also some adjustments to waybar style.css

--- .config/nvim/lua/plugins/lazy/none-ls.lua
+++ .config/nvim/lua/plugins/lazy/none-ls.lua
@@ -7,6 +7,7 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettierd,
+                null_ls.builtins.formatting.pg_format,
             }
         })
     end
